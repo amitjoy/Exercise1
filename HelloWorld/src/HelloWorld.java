bb65eba5799fdd5ac42d4374eeb392582f4318a5
Amit Kumar Mondal <admin@amitinside.com>
2015-10-24 19:22:58 +0200

    Minor Refactoring Added Author Info

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,22 +1,37 @@
+
+/*******************************************************************************
+ * Copyright 2015 TU Munich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 import java.util.Optional;
 import java.util.Scanner;
 
 /**
+ * Main Application Client
  *
- */
-
-/**
  * @author Tobias
- *
+ * @author AMIT KUMAR MONDAL
  */
-public class HelloWorld {
+public final class HelloWorld {
 
 	/**
 	 * @param args
 	 */
 	public static void main(final String[] args) {
 		System.out.println("Hello ASE2015 - how are you? :)");
-		try (final Scanner sc = new Scanner(System.in);) {
+		try (final Scanner sc = new Scanner(System.in)) {
+			System.out.println("Please Enter Your Name: ");
 			final Optional<String> input = Optional.of(sc.nextLine());
 			HelloUser.greetUser(input);
 		}
