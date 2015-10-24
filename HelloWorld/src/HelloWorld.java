59e9c92bcd651c03119b2dbab1c093fd3982935f
Amit Kumar Mondal <admin@amitinside.com>
2015-10-24 13:27:26 +0200

    Added HelloUser Class Added Functionality to greet user

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,5 +1,8 @@
+import java.util.Optional;
+import java.util.Scanner;
+
 /**
- * 
+ *
  */
 
 /**
@@ -11,8 +14,12 @@ public class HelloWorld {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		System.out.println("Hello ASE2015 - how are you? :)");
+		try (final Scanner sc = new Scanner(System.in);) {
+			final Optional<String> input = Optional.of(sc.nextLine());
+			HelloUser.greetUser(input);
+		}
 	}
 
 }
